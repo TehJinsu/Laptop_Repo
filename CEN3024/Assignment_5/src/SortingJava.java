80adaa68b23e1dd9138d6c834acb8ba3fd9df9e3
TehJinsu <112648902+TehJinsu@users.noreply.github.com>
2024-02-24 10:33:15 -0500

    UpdatesToSortingJavaProject

--- CEN3024/Assignment_5/src/SortingJava.java
+++ CEN3024/Assignment_5/src/SortingJava.java
@@ -2,11 +2,27 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Random;
 
 public class SortingJava {
 	public static void main(String args[]) throws FileNotFoundException, IOException{
+		Random rand = new Random();
+		
+		try {
+			PrintWriter writer = new PrintWriter("millionsofnumbers.txt");
+			
+			for (int i = 0; i < 1000000; i++) {
+				writer.println(rand.nextInt(10000000));
+			}
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println("Done!");
+		
 		long startTime = System.currentTimeMillis();
 		ArrayList<String> nums = new ArrayList<String>();  
 		try (BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\ivanl\\Documents\\GitHub\\Laptop_Repo\\CEN3024\\Assignment_5\\src\\millionsofnumbers.txt"))) {
